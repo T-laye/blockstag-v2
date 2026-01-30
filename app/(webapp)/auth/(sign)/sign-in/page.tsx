ec680b522a189d5e1e7ff626b7fa776289bc9ce3
Tubolayefa <tlayefaah@gmail.com>
2026-01-30 11:05:20 +0100

    adjusted route

--- app/(webapp)/auth/(sign)/sign-in/page.tsx
+++ app/(webapp)/auth/(sign)/sign-in/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import SignInForm from "../../../../../components/auth/SignInForm";
+import SignInForm from "../../../../../components/auth/signInForm";
 
 export default function Page() {
   return (
