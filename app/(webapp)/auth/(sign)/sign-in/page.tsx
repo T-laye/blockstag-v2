268a6b31a29a3ab9a1cab36fbe5e86cf74e99084
Tubolayefa <tlayefaah@gmail.com>
2026-01-30 11:21:14 +0100

    sign in

--- app/(webapp)/auth/(sign)/sign-in/page.tsx
+++ app/(webapp)/auth/(sign)/sign-in/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import SignInForm from "../../../../../components/auth/SignInForm";
 // import SignInForm from "../../../../../components/auth/SignInForm";
 
 export default function Page() {
@@ -24,7 +25,7 @@ export default function Page() {
         </p>
       </div>
       <div>
-        {/* <SignInForm /> */}
+        <SignInForm />
       </div>
     </div>
   );
